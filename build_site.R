924a7a727b69eb2b7c85ee21d42822a97462ea7a
Rosanna Neuhausler <rneuhausler@berkeley.edu>
2021-03-22 11:51:11 -0400

    updated text

--- build_site.R
+++ build_site.R
@@ -1,7 +1,7 @@
 #install.packages("rmarkdown", type = "source")
 
 wd = getwd()
-setwd("/Users/rosannaneuhausler/Desktop/website/rneuhausler.github.io")
+setwd("/Users/rosannaneuhausler/Desktop/job_apps/rneuhausler.github.io/")
 
 #render your sweet site. 
 rmarkdown::render_site()
